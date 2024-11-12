f1bb85fba21f19eb52f16c2ee781ee2784349c40
Cameron Terry <cwterry314@gmail.com>
2024-11-12 06:42:12 -0500

    Get all emojis (#2)

--- README.md
+++ README.md
@@ -5,4 +5,7 @@ What does it look like when you group similar emojis on a 2D graph
 in `v0`, run `source dev_setup.sh`. This installs a code linter [ruff](https://github.com/astral-sh/ruff) for pre-commit checks
 
 ### testing
-`coverage run -m pytest  -v -s` for unit tests
+`coverage run -m pytest  -v -s && coverage report -m`
+for unit tests coverage must be above 0.9
+
+[excluding code coverage](https://coverage.readthedocs.io/en/latest/excluding.htmlhttps://coverage.readthedocs.io/en/latest/excluding.html)
--- v0/retrieval/tests/test_get_emoji_data.py
+++ v0/retrieval/tests/test_get_emoji_data.py
@@ -1,6 +1,62 @@
+import pytest
 from retrieval import get_all_emojis
+from retrieval import config as c
 
 
 def test_get_emoji_data():
     first_place_medal = get_all_emojis.data_given("🥇")
     assert first_place_medal["en"] == ":1st_place_medal:"
+
+
+def test_all_emojis():
+    emojis = get_all_emojis.all_emojis()
+    assert len(emojis) > 0
+
+
+def test_emoji_map():
+    emoji_map = get_all_emojis.emoji_map()
+    assert emoji_map["🥇"] == ":1st_place_medal:"
+
+
+def test_emoji_filter_by_status():
+    emoji_map = get_all_emojis.emoji_map()
+    emojis = get_all_emojis.emoji_filter_by_status(emoji_map, "fully_qualified")
+    assert len(emojis) > 0
+
+
+def test_emoji_filter_by_status_bad_input():
+    emoji_map = get_all_emojis.emoji_map()
+    with pytest.raises(ValueError):
+        get_all_emojis.emoji_filter_by_status(emoji_map, "bad_input")
+
+
+def test_all_emojis_with_data_write():
+    emoji_map = get_all_emojis.all_emojis_with_data(
+        f"./retrieval/{c.RET_SAVE_DATA_FOLDER}", "w"
+    )
+    assert len(emoji_map) > 0
+
+
+def test_all_emojis_with_data_read():
+    get_all_emojis.all_emojis_with_data(f"./retrieval/{c.RET_SAVE_DATA_FOLDER}", "w")
+
+    emoji_map = get_all_emojis.all_emojis_with_data(
+        f"./retrieval/{c.RET_SAVE_DATA_FOLDER}", "r"
+    )
+    assert len(emoji_map) > 0
+
+
+def test_all_emojis_with_data_filter(filters=["fully_qualified"]):
+    get_all_emojis.all_emojis_with_data(f"./retrieval/{c.RET_SAVE_DATA_FOLDER}", "w")
+
+    emoji_map = get_all_emojis.all_emojis_with_data(
+        f"./retrieval/{c.RET_SAVE_DATA_FOLDER}", "r", filters
+    )
+    assert len(emoji_map) > 0
+
+
+def test_bad_input():
+    with pytest.raises(ValueError):
+        get_all_emojis.all_emojis_with_data(
+            f"./retrieval/{c.RET_SAVE_DATA_FOLDER}", "x"
+        )
